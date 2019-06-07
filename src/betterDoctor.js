51880cef7d9af040d1e503cd0df1e5412f439569
colton lacey <coltnutmiles14@gmail.com>
2019-06-07 11:28:05 -0700

    add back end logic in betterdoctor JS

--- src/betterDoctor.js
+++ src/betterDoctor.js
@@ -1,45 +1,20 @@
-import $ from 'jquery';
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css';
-// import './styles.css';
-import { BetterDoctor } from './findABar';
+export class FindABar {
+  getBar(state, name, city) {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      // let url = `https://api.openbrewerydb.org/breweries/search?query=${bar}`
+      let url = `https://api.openbrewerydb.org/breweries?by_state=${state}&by_name=${name}&by_city=${city}`
 
-$(document).ready(function() {
-  $('#bar').submit(function(event) {
-    event.preventDefault();
-    let state = $('#state').val();
-    let name = $('#name').val();
-    let city = $('#city').val();
-    $('#state').val("");
-    $('#name').val("");
-    $('#city').val("");
-
-    let findABar = new FindABar();  // create instance of WeatherService class
-    findABar.getBar(state, name, city).then((response) => {
-      let text = JSON.parse(response);
-      $("#output").append(`<h1>Bar Search Results:</h1><br>`);
-      text.forEach((bar) => {
-        console.log(bar);
-        $("#output").append(`<h1> Name: ${bar.name}</h1>`);
-        $("#output").append(`<h1> Brew Type: ${bar.brewery_type}</h1>`);
-        $("#output").append(`<h1> Street Adress: ${bar.street}</h1>`);
-        $("#output").append(`<h1> City: ${bar.city}</h1>`);
-        $("#output").append(`<h1> State: ${bar.state}</h1>`);
-        $("#output").append(`<h1> Phone-Number: ${bar.phone}</h1><br><br>`);
-
-        //   $("#output").html(`<h1> ${body}</h1>`)
-
-      });
-
-    });  // call the instance method and pass in user input
-
-    // promise.then(function(response) {
-    //
-    //
-    //
-    // }, function(error) {
-    //   $('.showErrors').text(`There was an error processing your request: ${error.message}`);
-    // });
-  });
-
-});
+      request.onload = function() {
+        if (this.status === 200) {
+          //return(request.response);
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
---
+++ src/main.js
@@ -0,0 +1,45 @@
+import $ from 'jquery';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './styles.css';
+import { BetterDoctor } from './findABar';
+
+$(document).ready(function() {
+  $('#bar').submit(function(event) {
+    event.preventDefault();
+    let state = $('#state').val();
+    let name = $('#name').val();
+    let city = $('#city').val();
+    $('#state').val("");
+    $('#name').val("");
+    $('#city').val("");
+
+    let findABar = new FindABar();  // create instance of WeatherService class
+    findABar.getBar(state, name, city).then((response) => {
+      let text = JSON.parse(response);
+      $("#output").append(`<h1>Bar Search Results:</h1><br>`);
+      text.forEach((bar) => {
+        console.log(bar);
+        $("#output").append(`<h1> Name: ${bar.name}</h1>`);
+        $("#output").append(`<h1> Brew Type: ${bar.brewery_type}</h1>`);
+        $("#output").append(`<h1> Street Adress: ${bar.street}</h1>`);
+        $("#output").append(`<h1> City: ${bar.city}</h1>`);
+        $("#output").append(`<h1> State: ${bar.state}</h1>`);
+        $("#output").append(`<h1> Phone-Number: ${bar.phone}</h1><br><br>`);
+
+        //   $("#output").html(`<h1> ${body}</h1>`)
+
+      });
+
+    });  // call the instance method and pass in user input
+
+    // promise.then(function(response) {
+    //
+    //
+    //
+    // }, function(error) {
+    //   $('.showErrors').text(`There was an error processing your request: ${error.message}`);
+    // });
+  });
+
+});
